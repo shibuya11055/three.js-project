d2f7da5d372ff6b496311fbf3d44c677a388509d
kyohei.shibuya <shibuya01055@gmail.com>
2021-09-23 10:54:34 +0900

    vue componentの作成

---
+++ src/views/Index.vue
@@ -0,0 +1,14 @@
+<template>{{ test }}</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Index",
+  setup() {
+    return {
+      test: "test",
+    };
+  },
+});
+</script>
